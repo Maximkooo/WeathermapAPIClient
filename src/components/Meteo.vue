1df4e52bd0c0788f17faa592ce25ea47f5b0d385
Maximkooo <43643606+Maximkooo@users.noreply.github.com>
2022-03-10 00:46:15 +0100

    Finish

--- src/components/Meteo.vue
+++ src/components/Meteo.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div class="container">
     <h1 class="my-4 text-center ">Weathermap API Client</h1>
@@ -14,17 +13,17 @@
       />
       <label for="position" class="label2">Or push button</label>
       <button v-on:click="goMeteoRandom">Random</button>
-      <h1 v-if="error">Vue is awesome!</h1>
+      <div class="area" v-if="error">⚠ wrong input ⚠</div>
     </div>
     <div class="w-75 m-auto" v-if="temps">
-      <h3 class="text-center mb-3" v-if="temps.name !== ''">Location : <b>{{ temps.name }}</b>,<b>{{ temps.sys.country}}</b></h3>
+      <h3 class="text-center mb-3" v-if="temps.name !== ''">Location : <b>{{ temps.name }}</b>,<b>{{ temps.sys.country }}</b></h3>
       <h3 class="text-center mb-3">Lat. : <b>{{ temps.coord.lat }}</b> Lon. : <b>{{ temps.coord.lon }}</b></h3>  
       <div class="card text-center p-5" style="border-radius:100px">
-        <p class="texte-affichage">
-          Temperature : <b>{{ temps.main.temp.toFixed() }}°</b>
+        <p class="texte-affichage ">
+          Temperature : <b>{{ Math.round(temps.main.temp) }}°</b>
         </p>
         <p class="texte-affichage">
-          Temps : <b>{{ temps.weather[0].description }}</b>
+          Description : <b>{{ temps.weather[0].description }}</b>
         </p>
       </div>
     </div>
@@ -44,26 +43,47 @@ export default {
       lat: 0,
       lon: 0,
       error: false,
-      cache: []
+      cache: {},
     };
   },
   
   methods: {
-    goMeteo(e) {
-      if (e.key === "Enter") {
-      Axios.get(   
-        `${this.urlApi}q=${this.requete}&appid=${this.apiKey}&lang=en&units=metric`
-      ).then((response) => {
-        this.temps = response.data;
-        //console.log(this.temps)   
-      }).catch(
-          error => {
-            console.log(error);
-            this.error = !this.error            
-      });
+    catchMeteo(){
+      this.temps.name = this.cache[this.requete][0];
+      this.temps.sys.country = this.cache[this.requete][1];
+      this.temps.weather[0].description = this.cache[this.requete][2];
+      this.temps.main.temp = this.cache[this.requete][3];
+      this.temps.coord.lat = this.cache[this.requete][4];
+      this.temps.coord.lon = this.cache[this.requete][5];
+
       this.requete = "";
       this.error = false;
-      }
+    },
+
+    goMeteo(e) {
+      
+      if (e.key === "Enter" && typeof this.cache[this.requete] === "undefined") {
+
+        Axios.get(   
+          `${this.urlApi}q=${this.requete}&appid=${this.apiKey}&lang=en&units=metric`
+        ).then((response) => {
+          this.temps = response.data;
+          this.cache[this.requete.toLowerCase()] = [this.temps.name, this.temps.sys.country, 
+          this.temps.weather[0].description, this.temps.main.temp.toFixed(), this.temps.coord.lat, this.temps.coord.lon]; 
+
+          this.requete = "";
+        }).catch(
+            error => {
+              console.log(error);
+              this.error = !this.error      
+              this.requete = "";   
+        });
+        this.error = false;
+        }
+
+      if (e.key === "Enter" && typeof this.cache[this.requete] !== "undefined"){
+        this.catchMeteo();
+      }   
     },
 
     goMeteoRandom() {
@@ -73,8 +93,11 @@ export default {
         `${this.urlApi}lat=${this.lat}&lon=${this.lon}&appid=${this.apiKey}&lang=en&units=metric`
       ).then((response) => {
         this.temps = response.data;
+        this.cache[this.requete.toLowerCase()] = [this.temps.name, this.temps.sys.country, 
+        this.temps.weather[0].description, this.temps.main.temp.toFixed(), this.temps.coord.lat, this.temps.coord.lon]; 
+        this.requete = "";
       });
-      this.requete = "";
+      this.error = false;
     },
   },
 };
@@ -94,5 +117,39 @@ button{
 .label2{
   margin-top: 20px;
 }
+.area {
+  text-align: center;
+  font-size: 6.5em;
+  color: #fff;
+  letter-spacing: -7px;
+  font-weight: 700;
+  text-transform: uppercase;
+  animation: blur .75s ease-out infinite;
+  text-shadow: 0px 0px 5px #fff, 0px 0px 7px #fff;
+}
+
+@keyframes blur {
+  from {
+    text-shadow:0px 0px 10px #fff,
+      0px 0px 10px #fff, 
+      0px 0px 25px #fff,
+      0px 0px 25px #fff,
+      0px 0px 25px #fff,
+      0px 0px 25px #fff,
+      0px 0px 25px #fff,
+      0px 0px 25px #fff,
+      0px 0px 50px #fff,
+      0px 0px 50px #fff,
+      0px 0px 50px #7B96B8,
+      0px 0px 150px #7B96B8,
+      0px 10px 100px #7B96B8,
+      0px 10px 100px #7B96B8,
+      0px 10px 100px #7B96B8,
+      0px 10px 100px #7B96B8,
+      0px -10px 100px #7B96B8,
+      0px -10px 100px #7B96B8;
+  }
+}
+
 
 </style>
