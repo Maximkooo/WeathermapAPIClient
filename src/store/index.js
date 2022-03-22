bad0eb38554b1840d9ba7d01d9176b8e58e5a1ce
Maksym Synytskyi <43643606+Maximkooo@users.noreply.github.com>
2022-03-22 16:38:43 +0100

    fix

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,45 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    count: 0
+  	weather: {
+      id: null,
+      main: {
+        temp: null,
+      },
+    },
   },
   mutations: {
-    increment(state) {
-      state.count++;
-    }
+  	SET_WEATHER(state, payload) {
+  		state.weather = payload;
+  	},
   },
   actions: {
-    increment: ({ commit }) => commit('increment')
-  }
+  	loadWeather({ commit }, city) {
+      return new Promise((resolve, reject) => {
+        axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=54c6208e65525722577789e191782281`)
+  			.then((response) => {
+            commit('SET_WEATHER', response.data);
+            console.log('DATA', response.data);
+            resolve(response.data);
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      });
+  	},
+
+    loadByLocation({ commit, dispatch }, position) {
+      axios.get(`http://api.openweathermap.org/geo/1.0/reverse?lat=${position.coords.latitude}&lon=${position.coords.longitude}&limit=20&appid=54c6208e65525722577789e191782281`)
+        .then((res) => {
+          dispatch('loadWeather', res.data[0].name);
+        });
+  	},
+  },
+  modules: {
+  },
 });
